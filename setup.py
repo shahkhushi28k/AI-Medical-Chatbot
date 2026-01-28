8ba988e7d090630cc8f0c30119c55b0a4cdbc52b
Khushi Shah <shahkhushi.28k@gmail.com>
2026-01-28 13:07:09 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "edical_chatbot",
+    version='0.1.0',
+    author='Khushi Shah',
+    author_email='shakhushi.28k@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
